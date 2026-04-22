f753da9eec6e24235df5d8d1558976ac7067b50b
Ray Camurça <raycamurca7@gmail.com>
2026-04-22 17:55:09 -0400

    update

---
+++ p007/script.js
@@ -0,0 +1,97 @@
+// icone erro e spanErro
+
+const iconErro = '<i class="fa-solid fa-circle-xmark"></i>';
+const spanErro = document.querySelector(".erro");
+
+// Controle das tabs (navegação)
+
+const btTabs = document.querySelectorAll(".areas button");
+
+btTabs.forEach(botao =>{
+    botao.addEventListener("click", ()=>{
+        btTabs.forEach(tab =>{
+            tab.classList.remove("ativo");
+        })
+        botao.classList.add("ativo");
+
+        const conteudos = document.querySelectorAll(".conteudo");
+        conteudos.forEach(conteudo => conteudo.classList.remove("show"));
+        
+        const tabId = botao.getAttribute("conteudo-id");
+        const tabAtiva = document.getElementById(tabId);
+        tabAtiva.classList.add("show");
+    })
+})
+
+// click do botao adicionar
+
+const btAddTarefa = document.querySelector(".bt-add-tarefa");
+const input = document.querySelector("#itarefa");
+
+btAddTarefa.addEventListener("click", ()=>{
+    const inputValue = input.value.trim();
+    if(inputValue === ""){
+        return spanErro.innerHTML = `${iconErro} Nome da tarefa obrigatório`
+    };
+
+    tarefaTodas(inputValue);
+    tarefaPendentes(inputValue);
+})
+
+// Função criar tarefa
+
+const criarTarefa = (value) => {
+    const divCardTarefa = document.createElement("div");
+    divCardTarefa.setAttribute("class", "card-tarefa pendente");
+
+    const divTarefa = document.createElement("div");
+    divTarefa.setAttribute("class", "tarefa");
+    divCardTarefa.appendChild(divTarefa);
+
+    const divCardIcon = document.createElement("div"); 
+    divCardIcon.setAttribute("class", "card-icon");
+    divTarefa.appendChild(divCardIcon);
+
+    const iCirculo = document.createElement("i");
+    iCirculo.setAttribute("class", "fa-regular fa-circle");
+    divCardIcon.appendChild(iCirculo);
+
+    const pDescricao = document.createElement("p");
+    pDescricao.setAttribute("class", "descricao");
+    pDescricao.innerHTML = value;
+    divTarefa.appendChild(pDescricao);
+
+    const divCardDelete = document.createElement("div");
+    divCardDelete.setAttribute("class", "card-delete");
+    divCardTarefa.appendChild(divCardDelete);
+
+    const iLixeira = document.createElement("i");
+    iLixeira.setAttribute("class", "fa-regular fa-trash-can");
+    divCardTarefa.appendChild(iLixeira);
+
+    return divCardTarefa;
+}
+
+// add tarefa
+
+const tarefaTodas = (value) => {
+    const novaTarefa = criarTarefa(value);
+
+    const displayTodas = document.querySelector("#todas");
+    const msgPadraoVaziaTodas = document.querySelector(".conteudo-todos");
+    if(novaTarefa.classList.contains("card-tarefa")){
+        msgPadraoVaziaTodas.style.display = "none";
+        displayTodas.appendChild(novaTarefa);
+    }
+}
+
+const tarefaPendentes = (value) => {
+    const novaTarefa = criarTarefa(value);
+
+    const displayPendentes = document.querySelector("#pendentes");
+    const msgPadraoVaziaPendentes = document.querySelector(".conteudo-pendentes");
+    if(novaTarefa.classList.contains("pendente")){
+        msgPadraoVaziaPendentes.style.display = "none";
+        displayPendentes.appendChild(novaTarefa);
+    }
+}
